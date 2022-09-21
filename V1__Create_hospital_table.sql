3cfe386470ce57db29e92fab08807a59ad664549
notSoundBanana <chemaev.ds@gmail.com>
2022-09-21 12:17:56 +0300

    Edited V1

--- V1__Create_hospital_table.sql
+++ V1__Create_hospital_table.sql
@@ -1,15 +1,16 @@
 CREATE TABLE patient (
-    id BIGSERIAL,
-    fio VARCHAR(255),
-    gender VARCHAR(50),
-    birth_date DATE,
-    phone_number VARCHAR(20)
+    id BIGSERIAL PRIMARY KEY,
+    fio VARCHAR(255) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    birth_date DATE NOT NULL,
+    phone_number VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE doctor (
     id BIGSERIAL PRIMARY KEY,
     fio VARCHAR(255) NOT NULL,
-    gender VARCHAR(50) NOT NULL, speciality VARCHAR(50) NOT NULL,
+    gender VARCHAR(50) NOT NULL,
+    speciality VARCHAR(50) NOT NULL,
     birth_date DATE NOT NULL,
     phone_number VARCHAR(20) NOT NULL
 );
