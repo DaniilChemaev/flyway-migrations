39a2dbb159724760796c793617b7ffd2982044b8
Daniil Chemaev <chemaev.ds@gmail.com>
2022-10-18 22:08:09 +0300

    Edited Add_new_column_to_doctors

--- V7__Add_new_column_to_doctors.sql
+++ V7__Add_new_column_to_doctors.sql
@@ -1,7 +1,7 @@
 ALTER TABLE doctors ADD head_doctor varchar(100) DEFAULT null;
 
-INSERT INTO doctor (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Поддокторов Евгений Анатольевич', 'Мужской', 'Медбрат', '01.10.2000', '+79099099999', 1);
-INSERT INTO doctor (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подхирургов Павел Григорьевич', 'Мужской', 'Медбрат', '01.03.1999', '+79543251567', 2);
-INSERT INTO doctor (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подтерапевтов Юрий Максимович', 'Мужской', 'Медбрат', '10.05.1990', '+79987567878', 3);
-INSERT INTO doctor (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подстоматологина Ивана Николевна', 'Женский', 'Медсестра', '11.06.1998', '+79025768345', 4);
-INSERT INTO doctor (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подонкологов Александр Николаевич', 'Мужской', 'Медбрат', '10.01.2000', '+79058473829', 5);
+INSERT INTO doctors (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Поддокторов Евгений Анатольевич', 'Мужской', 'Медбрат', '01.10.2000', '+79099099999', 1);
+INSERT INTO doctors (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подхирургов Павел Григорьевич', 'Мужской', 'Медбрат', '01.03.1999', '+79543251567', 2);
+INSERT INTO doctors (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подтерапевтов Юрий Максимович', 'Мужской', 'Медбрат', '10.05.1990', '+79987567878', 3);
+INSERT INTO doctors (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подстоматологина Ивана Николевна', 'Женский', 'Медсестра', '11.06.1998', '+79025768345', 4);
+INSERT INTO doctors (fio, gender, speciality, birth_date, phone_number, head_doctor) VALUES ('Подонкологов Александр Николаевич', 'Мужской', 'Медбрат', '10.01.2000', '+79058473829', 5);
