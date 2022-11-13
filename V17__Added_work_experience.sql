151f5304b8db36cd8fe9f105c1ee1d685454ef7c
Daniil Chemaev <chemaev.ds@gmail.com>
2022-11-13 18:31:49 +0300

    V17__Added_work_experience

---
+++ V17__Added_work_experience.sql
@@ -0,0 +1,12 @@
+UPDATE doctors SET work_experience='10' WHERE fio='Поддокторов Евгений Анатольевич';
+UPDATE doctors SET work_experience='8' WHERE fio='Подхирургов Павел Григорьевич';
+UPDATE doctors SET work_experience='9' WHERE fio='Подтерапевтов Юрий Максимович';
+UPDATE doctors SET work_experience='10' WHERE fio='Подстоматологина Ивана Николевна';
+UPDATE doctors SET work_experience='7' WHERE fio='Подонкологов Александр Николаевич';
+UPDATE doctors SET work_experience='12' WHERE fio='Поддокторов Александр Анатольевич';
+
+UPDATE doctors SET work_experience='1' WHERE fio='Совсемподдокторов Евгений Анатольевич';
+UPDATE doctors SET work_experience='2' WHERE fio='Совсемподхирургов Павел Григорьевич';
+UPDATE doctors SET work_experience='2' WHERE fio='Совсемподтерапевтов Юрий Максимович';
+UPDATE doctors SET work_experience='1' WHERE fio='Совсемподстоматологина Ивана Николевна';
+UPDATE doctors SET work_experience='1' WHERE fio='Совсемподонкологов Александр Николаевич';
