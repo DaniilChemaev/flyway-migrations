503edf14fbc7a33aa7961b672db32b51522a002b
Daniil Chemaev <chemaev.ds@gmail.com>
2022-11-13 18:31:38 +0300

    V16__Edited_doctors_info

---
+++ V16__Edited_doctors_info.sql
@@ -0,0 +1,8 @@
+UPDATE doctors SET birth_date='1975-10-08' WHERE work_experience='3 года';
+UPDATE doctors SET birth_date='1970-10-08' WHERE work_experience='2 года';
+
+UPDATE doctors SET work_experience='20' WHERE work_experience='20 лет';
+UPDATE doctors SET work_experience='30' WHERE work_experience='30 лет';
+UPDATE doctors SET work_experience='13' WHERE work_experience='10 лет';
+UPDATE doctors SET work_experience='15' WHERE work_experience='3 года';
+UPDATE doctors SET work_experience='17' WHERE work_experience='2 года';
